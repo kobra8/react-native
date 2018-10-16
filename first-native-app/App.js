dc0cc1057977173e20b192c1b34ccf8d2f4489d4
jerzydrobot <george@codesushi.co>
2018-10-16 17:22:02 +0200

    Web view component

--- first-native-app/App.js
+++ first-native-app/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+    
+      <WebView
+      source={{uri: 'https://google.pl'}}
+      style={{ marginTop: 20 }}
+      />
+
     );
   }
 }
