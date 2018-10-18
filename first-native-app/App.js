443814d12c37c64d81a04f8dd49695845eb5c7a4
jerzydrobot <george@codesushi.co>
2018-10-18 11:18:39 +0200

    Sensors library

--- first-native-app/App.js
+++ first-native-app/App.js
@@ -1,16 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { WebView } from 'react-native';
+import { View, WebView, Text, Image, StyleSheet } from 'react-native';
+import { Gyroscope } from "react-native-sensors";
+
+const Dimensions = require("Dimensions");
+const PixelRatio = require("PixelRatio");
+const window = Dimensions.get("window");
+
+const deviceWidth = window.width;
+const deviceHeight = window.height;
+
+const imageWidth = 8 * deviceWidth;
+const imageHeight = deviceHeight;
+
+const middleOfTheScreenX = (imageWidth - deviceWidth) / 2;
+
+
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    new Gyroscope({
+      updateInterval: 50
+    })
+      .then(observable => {
+        observable.subscribe(({ y }) => {
+          this.setState(state => ({
+            y: y + state.y
+          }));
+        });
+      })
+      .catch(error => {
+        console.log("The sensor is not available!");
+      });
+
+      this.state = {
+        image: 'img/panorama.jpeg',
+        y: 0
+      };
+  }
   render() {
     return (
     
-      <WebView
-      source={{uri: 'https://google.pl'}}
-      style={{ marginTop: 20 }}
+      // <WebView
+      // source={{uri: 'https://google.pl'}}
+      // style={{ marginTop: 20 }}
+      // />
+      <View style={styles.container}>
+      <Image
+        translateX={positionOnScreenX + movementX}
+        style={styles.image}
+        source={{ uri: this.state.image }}
       />
-
+    </View>
     );
   }
 }
@@ -18,8 +60,15 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
+  image: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    height: imageHeight,
+    width: imageWidth
+  }
 });
